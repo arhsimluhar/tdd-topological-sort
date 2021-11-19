345985b2efaeceb5ca34784212bc0bb672799f6d
t_mishr <rahul.mishra@autodesk.com>
2021-11-19 12:43:47 +0530

    initial commit and test for single edge.

--- test_topological_sort.py
+++ test_topological_sort.py
@@ -5,3 +5,9 @@ def test_topological_sort_single_vertex():
     g = Graph(1)
     g.addEdge(1, 1)
     assert g.topological_sort() == [1]
+
+
+def test_topological_sort_single_edge():
+    g = Graph(2)
+    g.addEdge(1,2)
+    assert g.topological_sort() == [1, 2]
--- topological_sort.py
+++ topological_sort.py
@@ -5,13 +5,25 @@ class Graph:
     def __init__(self, vertices):
         self.graph = defaultdict(list)  # dictionary containing adjacency List
         self.V = vertices  # No. of vertices
+        self.edges = 0
 
     # function to add an edge to graph
+
     def addEdge(self, u, v):
         self.graph[u].append(v)
+        self.edges += 1
+
+    def num_of_edges(self):
+        return self.edges
 
     def topological_sort(self):
         stack = []
         if self.V == 1:
             stack = list(self.graph.keys())
+            return stack
+
+        if self.num_of_edges() == 1:
+            for i in self.graph:
+                stack.append(i)
+                stack.append(self.graph[i][0])
             return stack
