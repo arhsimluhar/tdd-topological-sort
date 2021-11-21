c50693507d4b98dbb99dc0ee09e7b924eb313b59
t_mishr <rahul.mishra@autodesk.com>
2021-11-22 01:53:26 +0530

    fix code for graph with indirect dependencies.

--- test_topological_sort.py
+++ test_topological_sort.py
@@ -26,7 +26,7 @@ def test_topological_sort_neighbour_points_back():
     g.addEdge(1, 2)
     g.addEdge(2, 1)
     with pytest.raises(SortException):
-        g.topological_sort()
+        assert g.topological_sort()
 
 
 def test_topological_sort_forms_loop():
@@ -35,7 +35,7 @@ def test_topological_sort_forms_loop():
     g.addEdge(2, 3)
     g.addEdge(3, 1)
     with pytest.raises(SortException):
-        g.topological_sort()
+        assert g.topological_sort() == [1, 2]
 
 
 def test_topological_sort_single_node_with_two_neighbours():
--- topological_sort.py
+++ topological_sort.py
@@ -9,30 +9,29 @@ class Graph:
     def __init__(self, vertices):
         self.graph = defaultdict(list)  # dictionary containing adjacency List
         self.V = vertices  # No. of vertices
-        self.edges = 0
 
     # function to add an edge to graph
 
     def addEdge(self, u, v):
         self.graph[u].append(v)
 
-    def topological_sort_helper(self, stack, visited, i):
+    def topological_sort_helper(self, stack, visited, neighbors, i):
+        visited[i] = True
+        neighbors[i] = True
         for j in self.graph[i]:
-            if i == j:
-                continue
-            if visited[j]:
-                raise SortException("Loop Exists")
-            else:
-                visited[j] = True
-                stack.append(j)
-                self.topological_sort_helper(stack, visited, j)
+            if not visited[j]:
+                self.topological_sort_helper(stack, visited, neighbors, j)
+            elif neighbors[j] and j != i:
+                raise SortException
+        stack.insert(0, i)
+        neighbors[i] = False
 
     def topological_sort(self):
         stack = []
         visited = [False] * (self.V + 1)
+        neighbors = [False] * (self.V + 1)
         for i in range(1, self.V + 1):
             if not visited[i]:
-                stack.append(i)
-                visited[i] = True
-                self.topological_sort_helper(stack, visited, i)
+                self.topological_sort_helper(stack, visited, neighbors, i)
+
         return stack
